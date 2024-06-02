b852b674f06d9401151870f380afda170ce96ce7
lukaskalab <lukas.kalab4@gmail.com>
2024-06-02 21:07:45 +0200

    Chat gpt verwendet um 9 weitere test cases zu schreiben. Diese waren nach implementierung gleich alle grün

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -66,4 +66,60 @@ describe("Gilded Rose", function() {
     expect(items[0].sell_in).toEqual(2);
   });
 
+  //AI test cases:
+
+  it("should degrade quality of 'Conjured' items twice as fast after sell_in", function() {
+    items = [ new Item("Conjured Mana Cake", 0, 6) ];
+    update_quality();
+    expect(items[0].quality).toEqual(2);
+  });
+
+  it("should degrade quality of normal items twice as fast after sell_in", function() {
+    items = [ new Item("+5 Dexterity Vest", 0, 20) ];
+    update_quality();
+    expect(items[0].quality).toEqual(18);
+  });
+
+  it("should not increase quality of 'Aged Brie' beyond 50", function() {
+    items = [ new Item("Aged Brie", 2, 50) ];
+    update_quality();
+    expect(items[0].quality).toEqual(50);
+  });
+
+  it("should drop quality of 'Backstage passes' to 0 after the concert", function() {
+    items = [ new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20) ];
+    update_quality();
+    expect(items[0].quality).toEqual(0);
+  });
+
+  it("should not change sell_in of 'Sulfuras'", function() {
+    items = [ new Item("Sulfuras, Hand of Ragnaros", 0, 80) ];
+    update_quality();
+    expect(items[0].sell_in).toEqual(0);
+  });
+
+  it("should not decrease quality of 'Sulfuras'", function() {
+    items = [ new Item("Sulfuras, Hand of Ragnaros", -1, 80) ];
+    update_quality();
+    expect(items[0].quality).toEqual(80);
+  });
+
+  it("should increase quality of 'Backstage passes' by 2 when sell_in is 10 days or less", function() {
+    items = [ new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20) ];
+    update_quality();
+    expect(items[0].quality).toEqual(22);
+  });
+
+  it("should increase quality of 'Backstage passes' by 3 when sell_in is 5 days or less", function() {
+    items = [ new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20) ];
+    update_quality();
+    expect(items[0].quality).toEqual(23);
+  });
+
+  it("should not change quality of 'Sulfuras' even after sell_in", function() {
+    items = [ new Item("Sulfuras, Hand of Ragnaros", -1, 80) ];
+    update_quality();
+    expect(items[0].quality).toEqual(80);
+  });
+
 });
