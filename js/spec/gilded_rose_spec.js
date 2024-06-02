09a751d323e8baae8131fda623315f37190bdff5
lukaskalab <lukas.kalab4@gmail.com>
2024-06-02 15:10:37 +0200

    erster test positiv

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -3,8 +3,8 @@ describe("Gilded Rose", function() {
   it("should decrease quality and sell_in at the end of the day for normal items", function() {
     items = [ new Item("+5 Dexterity Vest", 10, 20) ];
     update_quality();
-    expect(items[0].sell_in).toEqual("9");
-    expect(items[0].quality).toEqual("19");
+    expect(items[0].sell_in).toEqual(9);
+    expect(items[0].quality).toEqual(19);
   });
 
 });
