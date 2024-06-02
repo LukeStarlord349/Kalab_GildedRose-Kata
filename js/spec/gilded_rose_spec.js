54290c686760c734a2a77cb1d5ac660c657f7210
lukaskalab <lukas.kalab4@gmail.com>
2024-06-02 15:09:43 +0200

    ersten test geschrieben, er ist rot

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,9 +1,10 @@
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
+  it("should decrease quality and sell_in at the end of the day for normal items", function() {
+    items = [ new Item("+5 Dexterity Vest", 10, 20) ];
     update_quality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].sell_in).toEqual("9");
+    expect(items[0].quality).toEqual("19");
   });
 
 });
